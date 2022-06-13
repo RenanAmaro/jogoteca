a0e03f156f7c5e95e87d2d32fe2143de66bf278f
Renan <r.amaro86@gmail.com>
2022-06-13 19:58:55 -0300

    Criando a Página Novo Jogo

--- jogoteca.py
+++ jogoteca.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 
+# criar uma classe Jogo com os atributos nome, categoria, console
+class Jogo():
+    def __init__(self, nome, categoria, console) -> None:
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 def ola():
-    return render_template('lista.html')
+    j1 = Jogo('God of War', 'RPG', 'PS4')
+    j2 = Jogo('Fifa', 'Esporte', 'Xbox')
+    j3 = Jogo('Mortal Combate', 'Luta', 'PS2')
+    lista = [j1,j2,j3]
+    return render_template('lista.html',titulo='Jogos', jogos=lista)
 
-app.run(host='0.0.0.0', port=8080)
-# app.run()
+@app.route('/novojogo')
+def criar_novo_jogo():
+    pass
+
+# app.run(host='0.0.0.0', port=8080)
+app.run(debug=True)

da692e99a9a1b6e8a4da4e6fb4a999337e6ab294
Renan <r.amaro86@gmail.com>
2022-07-11 20:45:37 -0300

    Adicionando criptografia senhas

--- jogoteca.py
+++ jogoteca.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_wtf.csrf import CSRFProtect
+from flask_bcrypt import Bcrypt
 
 # criar uma classe Jogo com os atributos nome, categoria, console
 
 app = Flask(__name__)
-from views import *
+
 app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
+csrf= CSRFProtect(app)
+bcrypt = Bcrypt(app)
 
+from views_game import *
+from views_user import *
 if __name__ == '__main__':
     app.run(debug=True)
